9dd1739dcb5f678b954175d362b124d02933aa15
JD Tadlock <jt7903@gmail.com>
2024-04-05 13:08:23 -0400

    base started

---
+++ index.js
@@ -0,0 +1,13 @@
+const args = process.argv
+
+// console.log('browser stuff', document, window)
+const num1 = args[2]
+const num2 = args[3]
+
+console.log('The sum is:', +num1 + +num2)
+
+// Get two number arguments from the user's command list (process.argv)
+
+// Add those two numbers together
+
+// console.log the sum of the two numbers
