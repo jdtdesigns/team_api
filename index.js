6b4c41628fc15cf3487aade02713567654e3f182
JD Tadlock <jt7903@gmail.com>
2024-04-08 11:51:46 -0400

    finished basic modularization

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-const fs = require('fs')
+const printStudents = require('./lib/printStudents.js')
+const addStudent = require('./lib/addStudent.js')
 const studentName = process.argv[2]
 
 if (studentName === 'print') {
-  fs.readFile('./students.txt', 'utf8', (err, data) => {
-    if (err) {
-      return console.log(err)
-    }
-
-    const names = data.split('\n')
-
-    console.log('\nStudent List:', '\n', '----------')
-
-    for (let name of names) {
-      console.log(name)
-    }
-  })
+  printStudents()
 } else {
-  fs.appendFile('./students.txt', '\n' + studentName, (err) => {
-    if (err) {
-      return console.log(err)
-    }
-
-    console.log('Student has been added!')
-  })
+  addStudent(studentName)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// writeFile will overwrite a file's contents
-// fs.writeFile('./students.txt', studentName, (err) => {
-//   if (err) {
-//     return console.log(err)
-//   }
-
-//   console.log('Student has been added!')
-// })
-
-
-
-
