323bf361b87049fac62f07f52dc89fac1602bdb8
JD Tadlock <jt7903@gmail.com>
2024-04-11 10:08:55 -0400

    office hours oop example complete

--- index.js
+++ index.js
@@ -1,156 +1,30 @@
 
-class Phone {
-  screen = true
-
-  constructor(number, size, model, color) {
-    this.number = number
-    this.size = size
-    this.model = model
-    this.color = color
+class Animal {
+  constructor(legs, sound) {
+    this.legs = legs
+    this.sound = sound
   }
 
-  printModel() {
-    console.log('Model:', 'Base')
+  makeSound() {
+    console.log(this.sound)
   }
 }
 
-class iPhone extends Phone {
-  facetime = true
+class Bear extends Animal {
+  hibernates = true
 
-  constructor(number, size, model, color, appleID) {
-    super(number, size, model, color)
-
-    this.appleID = appleID
-  }
-
-  printModel() {
-    console.log('iPhone: ', this.model)
+  makeSound() {
+    console.log('Bear says', this.sound)
   }
 }
 
-class Samsung extends Phone {
-  foldable = true
-
-  constructor(number, size, model, color, repairId) {
-    super(number, size, model, color)
-
-    this.repairId = repairId
-  }
+class Tiger extends Animal {
+  isFeline = true
 }
 
-const jdPhone = new iPhone('777-777-7777', 'standard', '15', 'slate grey', 'aosidufoasidjfoqwiejf')
-const bobPhone = new iPhone('777-777-7777', 'standard', '12', 'green', 'qweoriuqwoeriuqweoriuq')
-
-const sarahPhone = new Samsung('777-777-7777', 'mini', 's23', 'blue', '234098023981023948')
+const grizzly = new Bear(4, 'grrrr')
+const tigger = new Tiger(4, 'rawrr')
 
 
-// console.log(jdPhone)
-// console.log(sarahPhone)
-
-jdPhone.printModel()
-
-sarahPhone.printModel()
-
-
-
-
-
-
-
-
-
-
-
-// function Person(name, age) {
-//   this.name = name
-//   this.age = age
-// }
-
-// Person.prototype.haveBirthday = function () {
-//   this.age++
-//   console.log('Happy Birthday', this.name)
-// }
-
-// class Person {
-//   // static species = 'homosapien'
-//   // species = 'homosapien'
-
-//   constructor(name, age, hobbies) {
-//     this.name = name
-//     this.age = age
-//     this.hobbies = hobbies
-//   }
-
-//   haveBirthday() {
-//     this.age++
-//     console.log('Happy Birthday', this.name)
-//   }
-
-//   printBirthday() {
-//     console.log(`${this.name} is ${this.age} years old.`)
-//   }
-
-//   // Create a method that loops over the hobbies prop array and prints out each hobby one by one to the terminal
-//   printHobbies() {
-//     console.log('\nHobbies\n---------')
-
-//     this.hobbies.forEach(hobby => {
-//       console.log(hobby)
-//     })
-//   }
-// }
-
-
-// const jd = new Person('JD', 44, ['fishing', 'pickleball'])
-
-// console.log(jd)
-// console.log(jd.species)
-
-// // jd.haveBirthday()
-// jd.printHobbies()
-
-
-
-
-
-
-
-
-
-
-
-// function Person(name, age, hobbies) {
-//   this.name = name
-//   this.age = age
-//   this.hobbies = hobbies
-// }
-// Person.sayHi = function () {
-//   console.log('Hi!')
-// }
-
-// Person.prototype.species = 'homosapien'
-
-// Person.prototype.haveBirthday = function () {
-//   this.age++
-//   console.log('Happy Birthday!')
-// }
-
-// Person.prototype.printBirthday = function () {
-//   console.log(`You are ${this.age} years old.`)
-// }
-
-// const jd = new Person('JD', 44, ['fishing', 'pickleball'])
-// const bob = new Person('Bob', 99, ['Bingo', 'Sitting'])
-// const sarah = new Person('Sarah', 40, ['movies', 'pickleball'])
-
-// console.log(jd)
-// console.log(bob.species)
-// console.log(sarah)
-
-// jd.haveBirthday()
-
-// jd.printBirthday()
-
-// bob.printBirthday()
-
-// Person.sayHi()
+grizzly.makeSound()
+tigger.makeSound()
