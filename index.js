0dfed60daf7f9ff0ceaad8757c0841ebec0abdf1
JD Tadlock <jt7903@gmail.com>
2024-04-15 10:47:43 -0400

    node requests complete

--- index.js
+++ index.js
@@ -1,37 +1,97 @@
-const fs = require('fs/promises')
-require('colors')
 
-async function getNames() {
-  const names = await fs.readFile('./names.txt', 'utf8')
 
-  return names
-}
+const tasks = [
+  {
+    text: 'task one',
+    priority: true
+  },
+  {
+    text: 'task two',
+    priority: true
+  },
+  {
+    text: 'task three',
+    priority: false
+  }
+]
 
-async function spinWheel() {
-  const nameData = await getNames()
-  const names = nameData.trim().split('\n')
-  const getRandom = () => {
-    const index = Math.floor(Math.random() * names.length)
-    return names[index]
+class TaskListItem {
+  constructor(text, priority) {
+    this.text = text
+    this.priority = priority
   }
 
-  let count = 30
-
-  const cycle = setInterval(() => {
-    const randomName = getRandom()
-
-    console.clear()
-
-    count--
-
-    if (!count) {
-      console.log(`${randomName} has been chosen!`.bgMagenta.white)
-      clearInterval(cycle)
-    } else {
-      console.log(randomName.bgBrightGreen.white)
-    }
-  }, 75);
-
+  render() {
+    return `<li>${this.text}</li>`
+  }
 }
 
-spinWheel()
+const taskItems = tasks.map((taskObj) => {
+  return new TaskListItem(taskObj.text, taskObj.priority)
+})
+
+let taskHTML = ''
+
+taskItems.forEach(task => {
+  taskHTML += task.render()
+})
+
+console.log(taskHTML)
+
+
+
+// const axios = require('axios')
+// const inquirer = require('inquirer')
+
+// const baseURL = 'https://swapi.dev/api'
+
+// function outputSearchResults(data) {
+//   console.log('\nResults:\n---------')
+
+//   data.results.forEach(result => {
+//     console.log('Name:', result.name)
+//   })
+// }
+
+// async function makeRequest(data) {
+//   const url = `${baseURL}/${data.dataset.toLowerCase()}?search=${data.search}`
+
+//   const res = await axios.get(url)
+//   return res.data
+// }
+
+// async function getSearch(datasetObject) {
+//   const searchObject = await inquirer.prompt({
+//     name: 'search',
+//     message: 'Please type a search word for your chosen dataset.'
+//   })
+
+//   return {
+//     dataset: datasetObject.dataset,
+//     search: searchObject.search
+//   }
+// }
+
+// async function getDataset() {
+//   // Get the dataset choice
+//   const answerObj = await inquirer.prompt({
+//     name: 'dataset',
+//     type: 'list',
+//     message: 'Please select a dataset from the list.',
+//     choices: ['Films', 'People', 'Planets', 'Species', 'Starships', 'Vehicles']
+//   })
+
+//   return answerObj
+// }
+
+// getDataset()
+//   .then(getSearch)
+//   .then(makeRequest)
+//   .then(outputSearchResults)
+
+
+
+// // axios.get(baseURL + '/planets')
+// //   .then((res) => {
+// //     console.log(res.data)
+// //   })
