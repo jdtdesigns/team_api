72dd5983e8e6cb90053071c580ca7bc766b0444a
JD Tadlock <jt7903@gmail.com>
2024-04-17 13:24:44 -0400

    express json user app complete

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
-const { v4: generateID } = require('uuid');
-const data = require('../db/data')
+const { v4: generateID } = require('uuid')
+const fs = require('fs/promises')
+
+async function getData() {
+  const data = await fs.readFile('./db/users.json', 'utf8')
+
+  return JSON.parse(data)
+}
 
 // GET all users
-router.get('/users', (requestObj, responseObj) => {
+router.get('/users', async (requestObj, responseObj) => {
+  const data = await getData()
   const nameQuery = requestObj.query.name?.toLowerCase()
 
   if (nameQuery) {
@@ -16,7 +23,8 @@ router.get('/users', (requestObj, responseObj) => {
 })
 
 // GET user by ID
-router.get('/users/:id', (requestObject, responseObject) => {
+router.get('/users/:id', async (requestObject, responseObject) => {
+  const data = await getData()
   const paramId = requestObject.params.id
 
   const user = data.find(uObj => uObj.id == paramId)
@@ -24,23 +32,40 @@ router.get('/users/:id', (requestObject, responseObject) => {
   responseObject.json(user || { message: 'User not found by that ID' })
 })
 
-// POST add a user
-router.post('/users/form', (requestObject, responseObject) => {
-  console.log(requestObject.body)
-
-  responseObject.redirect('/')
-})
-
-router.post('/users', (requestObject, responseObject) => {
+// POST - Add a user
+router.post('/users', async (requestObject, responseObject) => {
   const id = generateID()
+  const data = await getData()
 
   data.push({
     ...requestObject.body,
     id: id
   })
 
+  await fs.writeFile('./db/users.json', JSON.stringify(data, null, 2))
+
   responseObject.json({
     message: 'User has been added!'
+  })
+})
+
+// DELETE a user
+router.delete('/users/:id', async (requestObj, responseObj) => {
+  const users = await getData()
+  const id = requestObj.params.id
+
+  const filtered = users.filter(uObj => uObj.id !== id)
+
+  if (users.length > filtered.length) {
+    await fs.writeFile('./db/users.json', JSON.stringify(filtered, null, 2))
+
+    return responseObj.json({
+      message: `User with ID of ${id} deleted successfully!`
+    })
+  }
+
+  responseObj.json({
+    message: 'A user with that ID cannot be found.'
   })
 })
 
