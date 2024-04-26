ff1df7b412bd7f72bdd5167ddec115375698f528
JD Tadlock <jt7903@gmail.com>
2024-04-26 14:16:28 -0400

    reverted connection

--- db/client.js
+++ db/client.js
@@ -1,10 +1,7 @@
 const { Sequelize } = require('sequelize');
 const is_prod = process.env.NODE_ENV // 'production'
 
-const client = is_prod ? new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false
-}) :
+const client = is_prod ? new Sequelize(process.env.DATABASE_URL) :
   new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
